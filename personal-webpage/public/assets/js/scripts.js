68cfb6376a333e10175c30cd98d68d460048859b
YUEWU1233 <148975631+YUEWU1233@users.noreply.github.com>
2025-05-14 23:02:56 +0200

    Add files via upload

--- personal-webpage/public/assets/js/scripts.js
+++ personal-webpage/public/assets/js/scripts.js
@@ -49,3 +49,40 @@ document.addEventListener("click", () => {
   const menu = document.getElementById("miniContactMenu");
   if (menu) menu.classList.remove("show");
 });
+
+// 图片轮播及左右箭头
+function startGalleryCarousel(imgList, carouselId, interval = 2500) {
+  let idx = 0;
+  const carousel = document.getElementById(carouselId);
+  if (!carousel) return;
+  const img = carousel.querySelector(".carousel-img");
+  const left = carousel.querySelector(".left-arrow");
+  const right = carousel.querySelector(".right-arrow");
+  let timer;
+
+  function showImg(newIdx) {
+    idx = (newIdx + imgList.length) % imgList.length;
+    img.src = imgList[idx];
+    img.alt = "Gallery Photo " + (idx + 1);
+  }
+
+  function next() {
+    showImg(idx + 1);
+    resetTimer();
+  }
+  function prev() {
+    showImg(idx - 1);
+    resetTimer();
+  }
+  function resetTimer() {
+    if (timer) clearInterval(timer);
+    timer = setInterval(() => showImg(idx + 1), interval);
+  }
+
+  if (left && right) {
+    left.onclick = prev;
+    right.onclick = next;
+  }
+  showImg(idx);
+  timer = setInterval(() => showImg(idx + 1), interval);
+}
